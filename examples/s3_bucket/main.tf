9ef8e5e548a89fc8ed2ca2acc0bb5d8c42456878
Benjamin Clark <bclark@sudoblark.com>
2024-10-08 17:15:39 +0100

    Version bump for example

--- examples/s3_bucket/main.tf
+++ examples/s3_bucket/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 module "s3_bucket" {
 
-  source = "github.com/sudoblark/sudoblark.terraform.module.aws.s3_bucket?ref=1.0.1"
+  source = "github.com/sudoblark/sudoblark.terraform.module.aws.s3_bucket?ref=1.0.2"
 
   application_name = var.application_name
   environment      = var.environment
