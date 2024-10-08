b46f9b0e994760bbd69571bc038d3897ba8f19f0
Benjamin Clark <bclark@sudoblark.com>
2024-10-08 12:42:34 +0100

    Swap key ID to use ARN (#2)
    * Swap key ID to use ARN
    
    * Update example version

--- modules/s3_bucket/cmk.tf
+++ modules/s3_bucket/cmk.tf
@@ -1,6 +1,6 @@
 resource "aws_kms_key" "customer_managed_key" {
   count               = var.enable_cmk_kms ? 1 : 0
-  description         = "S3 key to encrypt contents within ${local.bucket_name}}"
+  description         = "S3 key to encrypt contents within ${local.bucket_name}"
   enable_key_rotation = true
 }
 
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "cmk" {
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.customer_managed_key[count.index].id
+      kms_master_key_id = aws_kms_key.customer_managed_key[count.index].arn
       sse_algorithm     = "aws:kms"
     }
   }
